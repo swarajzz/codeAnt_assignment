4344886878883c4f18565a0bd21d1a4c9247e759
Swaraj <sharma.swaraj0403@gmail.com>
2024-12-21 22:35:50 +0530

    feat(login): right section of the login page

---
+++ app/_components/AuthForm.js
@@ -0,0 +1,69 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import { Button } from "./button";
+import { buttonData } from "@/public/data";
+
+export default function AuthForm() {
+  const [selectedOption, setSelectedOption] = useState("saas");
+
+  const options = [
+    { label: "SAAS", value: "saas" },
+    { label: "Self Hosted", value: "self_hosted" },
+  ];
+
+  return (
+    <section className="bg-[#FAFAFA] w-full md:w-1/2 flex items-center justify-center">
+      <div className="w-full max-w-2xl mx-6 my-2 md:my-4 flex flex-col gap-5">
+        <div className="bg-white flex flex-col  border-[#D5D7DA] border-2 rounded-xl">
+          <div className="flex flex-col items-center gap-8 px-3 md:px-5 py-6 md:py-9 border-b-2 border-[#D5D7DA]">
+            <Image
+              src="/fullLogo.svg"
+              alt="Codeant logo"
+              width={201}
+              height={40}
+            />
+
+            <h1 className="text-2xl font-semibold lg:text-3xl">
+              Welcome to CodeAnt AI
+            </h1>
+
+            <div className="flex w-full">
+              {options.map((option) => (
+                <button
+                  key={option.value}
+                  onClick={() => setSelectedOption(option.value)}
+                  className={`w-full px-4 py-3 rounded-lg font-medium text-xl ${
+                    selectedOption === option.value
+                      ? "bg-accent-200 text-white"
+                      : "hover:bg-gray-100 bg-[#FAFAFA]"
+                  }`}
+                >
+                  {option.label}
+                </button>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center gap-3 md:gap-5 py-6 px-8">
+            {buttonData[selectedOption]?.map((button, index) => (
+              <Button
+                key={index}
+                size="lg"
+                intent="outline"
+                className="w-full max-w-[446px] flex gap-2 items-center justify-center"
+              >
+                <div className="w-[25px]">{button.icon}</div>
+                <span>{button.label}</span>
+              </Button>
+            ))}
+          </div>
+        </div>
+
+        <p className="text-center">
+          By signing up you agree to the <strong>Privacy Policy</strong>
+        </p>
+      </div>
+    </section>
+  );
+}
