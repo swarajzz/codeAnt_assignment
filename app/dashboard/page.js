fa8d1940d06333d9be3548abc0b3c7ed8b0b4800
Swaraj <sharma.swaraj0403@gmail.com>
2024-12-22 21:25:04 +0530

    fix(dashboard): style fixes + resposnive design styles

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -17,68 +17,79 @@ export default function Dashboard() {
   return (
     <main className="flex h-screen bg-[#FAFAFA]">
       <Sidebar />
-      <section className="flex-1 bg-white m-4 rounded-lg border-2 border-[#E9EAEB] overflow-auto">
-        <div className="p-6">
-          <div className="flex items-center justify-between mb-6">
-            <div className="flex flex-col">
-              <h1 className="text-2xl font-semibold">Repositories</h1>
+      <section className="flex-1 m-6 overflow-auto scrollbar-none">
+        <div className=" border overflow-hidden rounded-xl border-[#E9EAEB] bg-white">
+          <div className="p-6 rounded-t-xl border-b border-[#E9EAEB]">
+            <div className="flex items-start flex-col lg:flex-row lg:items-center gap-2 justify-between mb-4">
+              <div className="flex flex-col gap-1">
+                <h1 className="text-2xl text-primary-300 font-semibold">
+                  Repositories
+                </h1>
 
-              <span className="text-sm text-muted-foreground">
-                {repositories.length} total repositories
-              </span>
+                <span className="text-sm text-primary-100">
+                  {repositories.length} total repositories
+                </span>
+              </div>
+
+              <div className="flex gap-2 text-[#414651]">
+                <Button className="flex gap-2" intent="outline" size="sm">
+                  <RefreshCw width={20} height={20} />
+                  <span className="text-primary-100">Refresh All</span>
+                </Button>
+                <Button className="flex gap-2" size="sm">
+                  <Plus />
+                  <span>Add Repository</span>
+                </Button>
+              </div>
             </div>
 
-            <div className="flex gap-2 text-[#414651]">
-              <Button className="flex gap-2" intent="outline" size="md">
-                <RefreshCw width={20} height={20} />
-                <span>Refresh All</span>
-              </Button>
-              <Button className="flex gap-2" size="md">
-                <Plus />
-                <span>Add Repository</span>
-              </Button>
+            <div className="relative text-primary-100 max-w-[366px]">
+              <Search
+                className="absolute left-2 top-2.5 h-5 w-5"
+                color="#414651"
+              />
+              <Input
+                placeholder="Search Repositories"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="pl-9"
+              />
             </div>
           </div>
 
-          <div className="relative">
-            <Search className="absolute left-3 top-3 h-4 w-4" />
-            <Input
-              placeholder="Search Repositories"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              className="pl-9"
-            />
-          </div>
-        </div>
-
-        <div>
-          {filteredRepositories.map((repo) => (
-            <div
-              key={repo.name}
-              className="p-4 rounded-lg border bg-card hover:bg-accent/50 transition-colors"
-            >
-              <div className="flex items-center justify-between">
-                <div className="flex flex-col gap-4">
-                  <div className="flex items-center gap-2">
-                    <h2 className="font-medium text-xl">{repo.name}</h2>
-                    <span className="text-[14px] text-[#175CD3] border bg-[#EFF8FF] border-[#B2DDFF] rounded-full px-2 py-0.5">
-                      Public
-                    </span>
-                  </div>
-                  <div className="flex gap-5 text-base">
-                    <span className="flex items-center">
-                      <p className="">{repo.language}</p>
-                      <span className="ml-2 h-2 w-2 inline-flex rounded-full bg-[#1570EF]"></span>
-                    </span>
-                    <span className="flex items-center gap-1">
-                      <Database width={10.5} height={12} /> {repo.size}
-                    </span>
-                    <span>Updated {repo.updatedAt}</span>
+          <div className="rounded-b-xl">
+            {filteredRepositories.map((repo, index) => (
+              <div
+                key={repo.name}
+                className={`p-4 ${
+                  index !== 0 && "border-t border-[#D5D7DA]"
+                } hover:bg-accent/50 transition-colors`}
+              >
+                <div className="flex items-center justify-between">
+                  <div className="flex flex-col gap-2 sm:gap-4">
+                    <div className="flex items-center gap-2">
+                      <h2 className="font-medium text-lg sm:text-xl">
+                        {repo.name}
+                      </h2>
+                      <span className="text-[14px] text-[#175CD3] border bg-[#EFF8FF] border-[#B2DDFF] rounded-full px-2 py-0.5">
+                        Public
+                      </span>
+                    </div>
+                    <div className="flex gap-5 text-sm sm:text-base">
+                      <span className="flex items-center">
+                        <p className="">{repo.language}</p>
+                        <span className="ml-2 h-2 w-2 inline-flex rounded-full bg-[#1570EF]"></span>
+                      </span>
+                      <span className="flex items-center gap-1">
+                        <Database width={10.5} height={12} /> {repo.size}
+                      </span>
+                      <span>Updated {repo.updatedAt}</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </section>
     </main>
