246c32caeef9b833131b8a539b3b2a536f6edd61
Swaraj <sharma.swaraj0403@gmail.com>
2024-12-22 21:24:04 +0530

    update font to use Inter font

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
-  variable: "--font-geist-mono",
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${inter.variable} antialiased`}>
+      <body className={`${inter.variable} text-sm font-normal antialiased`}>
         {children}
       </body>
     </html>
