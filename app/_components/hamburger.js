168b6de34909a7b9c699e7d9843ab2b5746a562e
Swaraj <sharma.swaraj0403@gmail.com>
2024-12-22 23:27:14 +0530

    feat(hamburger): add hamburger nav for small screens

---
+++ app/_components/hamburger.js
@@ -0,0 +1,105 @@
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { AlignJustify, X } from "lucide-react";
+import fullLogo from "@/public/fullLogo.svg";
+import { sidebarItems, sidebarFooterItems } from "@/public/data";
+import { cloneElement } from "react";
+
+export default function Hamburger({ isOpen, toggleMenu }) {
+  const [activeButton, setActiveButton] = useState("/repositories");
+
+  const handleButtonClick = (route) => {
+    setActiveButton(route);
+  };
+
+  return (
+    <>
+      <div className="md:hidden border-b border-[#E9EAEB] fixed bg-white top-0 left-0 right-0 p-4 z-50">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Image
+              src={fullLogo}
+              unoptimized
+              alt="CodeAnt AI Logo"
+              width={161}
+              height={32}
+            />
+          </div>
+          <div className="cursor-pointer" onClick={toggleMenu}>
+            {isOpen ? <X /> : <AlignJustify />}
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`md:hidden absolute border border-[##E9EAEB] left-0 right-0 bg-white z-40 overflow-hidden transition-all duration-300 ease-in-out top-16`}
+        style={{
+          opacity: isOpen ? 1 : 0,
+          visibility: isOpen ? "visible" : "hidden",
+        }}
+      >
+        <div className="p-4 overflow-y-auto">
+          <select
+            defaultValue="default"
+            className="w-full text-base px-3 py-2 bg-background border border-[#D5D7DA] rounded-md focus:outline-none focus:ring-2 focus:ring-ring mb-4"
+          >
+            <option value="default">Swaraj</option>
+            <option value="org1">Organization 1</option>
+            <option value="org2">Organization 2</option>
+          </select>
+
+          <nav className="px-4">
+            <div className="flex flex-col gap-1.5">
+              {sidebarItems.map((item) => (
+                <Link
+                  className="flex text-lg"
+                  key={item.route}
+                  href={item.label === "Logout" ? "/login" : ""}
+                >
+                  <Button
+                    intent={activeButton === item.route ? "" : "ghost"}
+                    size={"md"}
+                    className="w-full flex justify-start gap-2"
+                    onClick={() => handleButtonClick(item.route)}
+                  >
+                    {cloneElement(item.icon, {
+                      className: "h-5 w-5",
+                    })}
+                    <span className="font-semibold"> {item.label} </span>
+                  </Button>
+                </Link>
+              ))}
+              {sidebarFooterItems.map((item) => (
+                <Link
+                  className="flex text-lg"
+                  key={item.route}
+                  href={item.label === "Logout" ? "/login" : ""}
+                >
+                  <Button
+                    intent={
+                      item.route === "/logout"
+                        ? "danger-ghost"
+                        : activeButton === item.route
+                        ? ""
+                        : "ghost"
+                    }
+                    size={"md"}
+                    className="w-full flex justify-start gap-2"
+                    onClick={() => handleButtonClick(item.route)}
+                  >
+                    {cloneElement(item.icon, {
+                      className: "h-5 w-5",
+                    })}
+                    <span> {item.label} </span>
+                  </Button>
+                </Link>
+              ))}
+            </div>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+}
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,25 +1,39 @@
-"use client";
-
 import { useState } from "react";
 import { RefreshCw, Plus, Search, Database } from "lucide-react";
 import { Button } from "../_components/ui/button";
 import { Input } from "../_components/ui/input";
 import Sidebar from "../_components/sidebar";
 import { repositories } from "@/public/data";
+import Hamburger from "../_components/hamburger";
 
 export default function Dashboard() {
+  const [isOpen, setIsOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
 
   const filteredRepositories = repositories.filter((repo) =>
     repo.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <main className="flex h-screen bg-[#FAFAFA]">
       <Sidebar />
       <section className="flex-1 m-6 overflow-auto scrollbar-none">
+        <Hamburger
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          toggleMenu={toggleMenu}
+        />
+        {isOpen && (
+          <div
+            className="md:hidden absolute inset-0 bg-black bg-opacity-50"
+            onClick={toggleMenu}
+          />
+        )}
         <div className=" border overflow-hidden rounded-xl border-[#E9EAEB] bg-white">
-          <div className="p-6 rounded-t-xl border-b border-[#E9EAEB]">
+          <div className="p-6 pt-16 md:p-6 rounded-t-xl border-b border-[#E9EAEB]">
             <div className="flex items-start flex-col lg:flex-row lg:items-center gap-2 justify-between mb-4">
               <div className="flex flex-col gap-1">
                 <h1 className="text-2xl text-primary-300 font-semibold">
@@ -72,7 +86,7 @@ export default function Dashboard() {
                         {repo.name}
                       </h2>
                       <span className="text-[14px] text-[#175CD3] border bg-[#EFF8FF] border-[#B2DDFF] rounded-full px-2 py-0.5">
-                        Public
+                        {!repo.isPublic ? "Private" : "Public"}
                       </span>
                     </div>
                     <div className="flex gap-5 text-sm sm:text-base">
